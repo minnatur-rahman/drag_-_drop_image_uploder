073196ea3a6eadc641658311b40486398e057e12
minnatur-rahman <minnatur7373@gmail.com>
2024-09-06 10:55:37 +0600

    alsdhj

---
+++ script.js
@@ -0,0 +1,9 @@
+const DragArea =   document.querySelector(".AppBody"),
+      DragText = DragArea.querySelector("h3"),
+      button = DragArea.querySelector("button"),
+      input = DragArea.querySelector("input");
+let myFile;   
+
+button.onclick = () => {
+    input.click();
+}
